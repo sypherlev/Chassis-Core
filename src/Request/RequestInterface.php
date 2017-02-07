c7eed0e1f008e2e258c27f4db575e5fdafd1f198
Claire Ryan <cryan@pcis.com>
2017-02-07 14:30:47 -0800

    Removed not needed function declaration

--- src/Request/RequestInterface.php
+++ src/Request/RequestInterface.php
@@ -6,6 +6,5 @@ namespace Chassis\Request;
 interface RequestInterface
 {
     public function insertData($name, $input);
-    public function transform($name, $data);
     public function getRawData($name);
 }
