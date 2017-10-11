8757818b62d7fd4f51340ee31b9d52bf1b79799f
Claire Ryan <cryan@pcis.com>
2017-10-11 13:39:27 -0700

    Cleaned up the database bootstrapper

--- src/Data/SourceBootstrapper.php
+++ src/Data/SourceBootstrapper.php
@@ -7,58 +7,31 @@ use SypherLev\Blueprint\QueryBuilders\Postgres\PostgresSource;
 
 class SourceBootstrapper
 {
-    public $driver;
-    public $host;
-    public $database;
-    public $user;
-    public $pass;
-    public $sanitizer_regex = '';
-
-    public function generateSource($identifier) {
-        $this->driver = isset($_ENV[$identifier.'_engine']) ? $_ENV[$identifier.'_engine'] : '';
-        $this->host = isset($_ENV[$identifier.'_host']) ? $_ENV[$identifier.'_host'] : '';
-        $this->database = isset($_ENV[$identifier.'_dbname']) ? $_ENV[$identifier.'_dbname'] : '';
-        $this->user = isset($_ENV[$identifier.'_username']) ? $_ENV[$identifier.'_username'] : '';
-        $this->pass = isset($_ENV[$identifier.'_password']) ? $_ENV[$identifier.'_password'] : '';
-        $this->sanitizer_regex = isset($_ENV[$identifier.'_sanitizer_regex']) ? $_ENV[$identifier.'_sanitizer_regex'] : '';
-        if($this->validateConfig()) {
-            $dns = $this->driver . ':dbname=' . $this->database . ";host=" . $this->host;
+    public static function generateSource($identifier) {
+        $driver = isset($_ENV[$identifier.'_engine']) ? $_ENV[$identifier.'_engine'] : '';
+        $host = isset($_ENV[$identifier.'_host']) ? $_ENV[$identifier.'_host'] : '';
+        $database = isset($_ENV[$identifier.'_dbname']) ? $_ENV[$identifier.'_dbname'] : '';
+        $user = isset($_ENV[$identifier.'_username']) ? $_ENV[$identifier.'_username'] : '';
+        $pass = isset($_ENV[$identifier.'_password']) ? $_ENV[$identifier.'_password'] : '';
+        try {
+            $dns = $driver . ':dbname=' . $database . ";host=" . $host;
             $options = [];
-            if($this->driver == 'mysql') {
+            if($driver == 'mysql') {
                 $options = array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
             }
-            $pdo = new \PDO($dns, $this->user, $this->pass, $options);
-            if($this->driver == 'mysql') {
+            $pdo = new \PDO($dns, $user, $pass, $options);
+            if($driver == 'mysql') {
                 return new MySqlSource($pdo);
             }
-            else if ($this->driver == 'pgsql') {
+            else if ($driver == 'pgsql') {
                 return new PostgresSource($pdo);
             }
             else {
                 throw (new \Exception("Unsupported database driver"));
             }
         }
-        else {
-            throw (new \Exception("Invalid or missing database connection parameters"));
+        catch (\Exception $e) {
+            throw (new \Exception("Could not connect to the database or create PDO: ". $e->getMessage()));
         }
-    }
-
-    private function validateConfig() {
-        if($this->driver == '') {
-            return false;
-        }
-        if($this->host == '') {
-            return false;
-        }
-        if($this->database == '') {
-            return false;
-        }
-        if($this->user == '') {
-            return false;
-        }
-        if($this->pass == '') {
-            return false;
-        }
-        return true;
     }
 }
