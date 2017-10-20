43fbecf38068b47929e9ae653b6fafa1f8eb734c
Claire Ryan <cryan@pcis.com>
2017-10-20 15:02:48 -0700

    Reverted changes to the bootstrapper, added the port config back in

--- src/Data/SourceBootstrapper.php
+++ src/Data/SourceBootstrapper.php
@@ -7,35 +7,61 @@ use SypherLev\Blueprint\QueryBuilders\Postgres\PostgresSource;
 
 class SourceBootstrapper
 {
-    public static function generateSource($identifier) {
-        $driver = isset($_ENV[$identifier.'_engine']) ? $_ENV[$identifier.'_engine'] : '';
-        $host = isset($_ENV[$identifier.'_host']) ? $_ENV[$identifier.'_host'] : '';
-        $database = isset($_ENV[$identifier.'_dbname']) ? $_ENV[$identifier.'_dbname'] : '';
-        $user = isset($_ENV[$identifier.'_username']) ? $_ENV[$identifier.'_username'] : '';
-        $pass = isset($_ENV[$identifier.'_password']) ? $_ENV[$identifier.'_password'] : '';
-        $port = isset($_ENV[$identifier.'_port']) ? $_ENV[$identifier.'_port'] : '';
-        try {
-            $dns = $driver . ':dbname=' . $database . ";host=" . $host;
-            if($port != "") {
-                $dns .= ";port=".$port;
-            }
+    public $driver;
+    public $host;
+    public $database;
+    public $user;
+    public $pass;
+    public $port;
+
+    public function generateSource($identifier) {
+        $this->driver = isset($_ENV[$identifier.'_engine']) ? $_ENV[$identifier.'_engine'] : '';
+        $this->host = isset($_ENV[$identifier.'_host']) ? $_ENV[$identifier.'_host'] : '';
+        $this->database = isset($_ENV[$identifier.'_dbname']) ? $_ENV[$identifier.'_dbname'] : '';
+        $this->user = isset($_ENV[$identifier.'_username']) ? $_ENV[$identifier.'_username'] : '';
+        $this->pass = isset($_ENV[$identifier.'_password']) ? $_ENV[$identifier.'_password'] : '';
+        $this->port = isset($_ENV[$identifier.'_port']) ? $_ENV[$identifier.'_port'] : '';
+        if($this->validateConfig()) {
+            $dns = $this->driver . ':dbname=' . $this->database . ";host=" . $this->host;
             $options = [];
-            if($driver == 'mysql') {
+            if($this->driver == 'mysql') {
                 $options = array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
             }
-            $pdo = new \PDO($dns, $user, $pass, $options);
-            if($driver == 'mysql') {
+            if($this->port != "") {
+                $dns .= ";port=".$this->port;
+            }
+            $pdo = new \PDO($dns, $this->user, $this->pass, $options);
+            if($this->driver == 'mysql') {
                 return new MySqlSource($pdo);
             }
-            else if ($driver == 'pgsql') {
+            else if ($this->driver == 'pgsql') {
                 return new PostgresSource($pdo);
             }
             else {
                 throw (new \Exception("Unsupported database driver"));
             }
         }
-        catch (\Exception $e) {
-            throw (new \Exception("Could not connect to the database or create PDO: ". $e->getMessage()));
+        else {
+            throw (new \Exception("Invalid or missing database connection parameters"));
         }
+    }
+
+    private function validateConfig() {
+        if($this->driver == '') {
+            return false;
+        }
+        if($this->host == '') {
+            return false;
+        }
+        if($this->database == '') {
+            return false;
+        }
+        if($this->user == '') {
+            return false;
+        }
+        if($this->pass == '') {
+            return false;
+        }
+        return true;
     }
 }
