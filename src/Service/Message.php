c868cd113393d0b92c472b3604af4b0dc709953a
Claire Ryan <cryan@pcis.com>
2018-06-05 11:07:38 -0700

    Added Message class updates

--- src/Service/Message.php
+++ src/Service/Message.php
@@ -1,15 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claireryan
- * Date: 2018-06-05
- * Time: 10:56 AM
- */
 
 namespace SypherLev\Chassis\Service;
 
-
 class Message
 {
+    private $is_normal = true;
+    private $error;
+    private $data;
+    private $status;
 
+    public function setData($data) {
+        $this->data = $data;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function isNormal() : bool {
+        return $this->is_normal;
+    }
+
+    public function hasProblem() {
+        $this->is_normal = false;
+    }
+
+    public function storeException(\Exception $e) {
+        $this->error = $e;
+    }
+
+    public function getException() : \Exception {
+        return $this->error;
+    }
+
+    public function getStatus() : string {
+        return $this->status;
+    }
+
+    public function setStatus(string $status) {
+        $this->status = $status;
+    }
 }
