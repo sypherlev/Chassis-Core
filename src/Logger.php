c11ccc00b957292b3b4719662e8402cadbd34ebd
Claire Ryan <cryan@pcis.com>
2018-06-12 17:00:01 -0700

    Added switch to log to Slack

--- src/Logger.php
+++ src/Logger.php
@@ -7,18 +7,30 @@ namespace SypherLev\Chassis;
 
 class Logger
 {
-    public static function store(string $message) {
+    public static function store(string $message, $destination = 'file') {
         $message = self::createLogMessage($message);
+        if($destination == 'slack') {
+            self::logToSlack($message);
+            return;
+        }
         self::logToFile($message);
     }
 
-    public static function storeException(\Exception $e) {
+    public static function storeException(\Exception $e, $destination = 'file') {
         $message = self::createLogMessage($e->getMessage().PHP_EOL.$e->getTraceAsString());
+        if($destination == 'slack') {
+            self::logToSlack($message);
+            return;
+        }
         self::logToFile($message);
     }
 
-    public function storeArray(array $e) {
+    public function storeArray(array $e, $destination = 'file') {
         $message = self::createLogMessage(print_r($e, true));
+        if($destination == 'slack') {
+            self::logToSlack($message);
+            return;
+        }
         self::logToFile($message);
     }
 
@@ -32,6 +44,32 @@ class Logger
             touch($logfile);
         }
         file_put_contents($logfile, $message, FILE_APPEND);
+    }
+
+    public static function logToSlack($message) {
+
+        $channel  = getenv('slack_channel');
+        $bot_name = 'Webhook';
+        $icon     = ':interrobang:';
+
+        $data = array(
+            'channel'     => $channel,
+            'username'    => $bot_name,
+            'text'        => $message,
+            'icon_emoji'  => $icon
+        );
+
+        $data_string = json_encode($data);
+
+        $ch = curl_init(getenv('slack_webhook'));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string))
+        );
+        curl_exec($ch);
     }
 
     public static function createLogMessage($message) {
