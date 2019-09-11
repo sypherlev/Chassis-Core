101a362234647c90d789958ccc225945494dda46
Claire Ryan <cryan@pcis.com>
2019-09-11 11:04:48 -0700

    Additional response tweaking

--- src/Response/ApiResponse.php
+++ src/Response/ApiResponse.php
@@ -9,11 +9,13 @@ class ApiResponse implements ResponseInterface
     private $httpcode = 404;
     private $message = 'No response found';
 
-    public function setHTTPCode($code) {
+    public function setHTTPCode($code)
+    {
         $this->httpcode = (int)$code;
     }
 
-    public function setOutputMessage($message) {
+    public function setOutputMessage($message)
+    {
         $this->message = $message;
     }
 
@@ -29,31 +31,31 @@ class ApiResponse implements ResponseInterface
         echo json_encode(array('message' => $this->message, 'data' => $this->data), JSON_NUMERIC_CHECK);
     }
 
-    public function dataResponse($label, $variable) {
-        if(!empty($variable)) {
-            // check if there is no data but the response is still valid
-            if(is_array($variable) && count($variable) == 0) {
-                $this->setHTTPCode(204);
-                $this->setOutputMessage('No data');
-            }
-            else {
-                $this->setHTTPCode(200);
-                $this->setOutputMessage('Data retrieved');
-            }
-            $this->insertOutputData($label, $variable);
+    public function dataResponse($label, $variable)
+    {
+        // check if there is no data but the response is still valid
+        if (is_array($variable) && count($variable) == 0) {
+            $this->setHTTPCode(204);
+            $this->setOutputMessage('No data');
+            $this->out();
+            return;
         }
-    else {
+        if (!empty($variable)) {
+            $this->setHTTPCode(200);
+            $this->setOutputMessage('Data retrieved');
+            $this->insertOutputData($label, $variable);
+        } else {
             $this->setHTTPCode(500);
             $this->setOutputMessage('Data not found');
         }
         $this->out();
     }
 
-    public function messageResponse($message, $isOkay = true) {
-        if($isOkay) {
+    public function messageResponse($message, $isOkay = true)
+    {
+        if ($isOkay) {
             $this->setHTTPCode(200);
-        }
-        else {
+        } else {
             $this->setHTTPCode(500);
         }
         $this->setOutputMessage($message);
